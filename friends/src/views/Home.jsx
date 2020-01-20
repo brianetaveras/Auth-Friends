3488a64374b4b883f44e6455b85b3cd93306de5f
brianetaveras <taveras574@gmail.com>
2020-01-20 17:28:45 -0500

    Added add button

--- friends/src/views/Home.jsx
+++ friends/src/views/Home.jsx
@@ -20,6 +20,21 @@ const Home = ()=> {
     }, [])
 
 
+    const AddButton = styled.button`
+        position: absolute;
+        bottom: 100px;
+        right: 100px;
+        height: 50px;
+        width: 50px;
+        border-radius: 50px;
+        background: #3CAEA3;
+        border: none;
+        box-shadow: 10px 10px 10px rgba(0,0,0,0.2);
+        font-size: 40px;
+        color: white;
+        cursor: pointer;
+
+    `
 
     return (
         <div className="home">
@@ -27,7 +42,7 @@ const Home = ()=> {
             {friends.length ? friends.map(friend=>{
                 return <FriendCard info={friend} setFriends={setFriends} key={friend.id} />
             }): 'You got not friends'}
-
+            <AddButton>+</AddButton>
         </div>
     );
 
